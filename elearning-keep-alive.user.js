4a74aba04b3e2202575feab45616afe7bd1517ab
Jianfeng Zhang <swordfeng123@gmail.com>
2016-09-08 13:47:46 +0800

    initial comit

---
+++ elearning-keep-alive.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Elearning Keep-Alive
+// @namespace    http://elearning.fudan.edu.cn/
+// @version      0.1
+// @description  Keep E-Learning Site alive!
+// @author       swordfeng
+// @match        http://elearning.fudan.edu.cn/*
+// @grant        none
+// ==/UserScript==
+'use strict';
+setInterval(keep_session_alive, 300000);
